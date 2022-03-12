c049b8f95ef0a504ab68d1b22d1a5d8e7753aa0b
Joey Cos <joeycosentino24@gmail.com>
2022-03-12 14:34:52 -0500

    initial commit

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,14 @@
-// Assignment code here
+// when I click the button for the password, I should be presented some prompts from which to choose password criteria
+
+// these prompts will let me choose the critera for my password (upper, lower, special, numerals)
+
+// User will also be prompted for length criteria (maybe array.length?), between 8 and 128
+
+// user input should be validated and at least one character type should be selected
+
+// after all prompts are answered, a password should be genereated.
+
+// when the password is generated, it should either be displayed in an alert or written on the page.
 
 
 // Get references to the #generate element
